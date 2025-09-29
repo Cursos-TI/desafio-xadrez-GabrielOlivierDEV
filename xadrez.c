d7840da6a2fb49b8212c5b994b5ae0ce8e07e53c
GabrielOlivierDEV <bieelzyolivier@gmail.com>
2025-09-29 18:13:02 -0300

    Nivel Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -7,37 +7,52 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int torre_numeroCasas = 5;
-    int bispo_numeroCasas = 5;
-    int rainha_numeroCasas = 8;
+    int torre_numeroCasas = 0;
+    int bispo_numeroCasas = 0;
+    int rainha_numeroCasas = 0;
+    int cavalo_numeroCasas = 0;
 
     // Implementação de Movimentação do Bispo
     // O bispo só pode se mover em diagonais, logo mostramos apenas essas direções.
 
     do {
         printf("\nBispo: Cima, Direita\n");
-        --bispo_numeroCasas;
-    } while (bispo_numeroCasas >= 1);
+        bispo_numeroCasas++;
+    } while (bispo_numeroCasas < 5);
 
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
-    while (torre_numeroCasas >= 1) {
+    while (torre_numeroCasas < 5) {
         printf("\nTorre: Direita\n");
-        torre_numeroCasas--;
+        torre_numeroCasas++;
     }
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
-    for (rainha_numeroCasas; rainha_numeroCasas >= 1; rainha_numeroCasas--) {
+    for (rainha_numeroCasas; rainha_numeroCasas < 8; rainha_numeroCasas++) {
         printf("\nRainha: Esquerda\n");
     }
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+
+    // Loop externo: vertical
+    for (cavalo_numeroCasas; cavalo_numeroCasas < 2; cavalo_numeroCasas++) {
+        printf("\nCavalo: Baixo\n");
+
+        // Loop interno: horizontal, roda apenas 1 vez
+        int j = 0;
+        do {
+            if (cavalo_numeroCasas == 1) { // garante que só rode no final do L
+                printf("\nCavalo: Esquerda\n");
+            }
+            j++;
+        } while (j < 1);
+    }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
